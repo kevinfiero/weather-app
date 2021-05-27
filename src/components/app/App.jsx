dd30c073d783859755b1198b6db08fdc7134a63c
kevinfiero <kjf46@cornell.edu>
2021-05-27 11:31:59 -0700

    add router

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../../containers/Home'
 
 export default function App() {
 
   return (
     <>
-      <Home/>
-
+    <Router>
+      <Switch>
+        <Route exact path="/" component = { Home } /> 
+      </Switch>
+    </Router>
     </>
   )
 }
