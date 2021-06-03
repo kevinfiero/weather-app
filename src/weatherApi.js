b3b2121e9497090c39bc99f2c4aa2f113cf6e62f
kevinfiero <kjf46@cornell.edu>
2021-06-03 12:24:46 -0700

    https

--- src/weatherApi.js
+++ src/weatherApi.js
@@ -1,43 +1,41 @@
 export const findCurrentWeather = (zipCode) => {
   return fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${process.env.API_KEY}`)
-  .then(res => res.json())
+    .then(res => res.json())
     .then(res => {
-      if(res.cod==="404"){
+      if (res.cod === "404") {
         return false
       } else {
-      return (
-        {
-          zipCode: zipCode,
-          city: res.name,
-          temperature: Math.round((res.main.temp - 273.15) * (9/5) + 32),
-          temperatureC: Math.round((res.main.temp - 273.15)),
-          icon: `https://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`,
-          condition: res.weather[0].main,
-          wind: Math.round(res.wind.speed),
-          windK: Math.round(res.wind.speed * 1.60934),
-          humidity: res.main.humidity
-        }
-      )}
+        return (
+          {
+            zipCode: zipCode,
+            city: res.name,
+            temperature: Math.round((res.main.temp - 273.15) * (9 / 5) + 32),
+            temperatureC: Math.round((res.main.temp - 273.15)),
+            icon: `https://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`,
+            condition: res.weather[0].main,
+            wind: Math.round(res.wind.speed),
+            windK: Math.round(res.wind.speed * 1.60934),
+            humidity: res.main.humidity
+          }
+        )
+      }
     })
 }
 
 export const findWeatherForecast = (zipCode) => {
   return fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}&appid=${process.env.API_KEY}`)
-  .then(res => res.json())
+    .then(res => res.json())
     .then(res => {
-      if(res.cod==="404"){
+      if (res.cod === "404") {
         return false
       } else {
-
-
         const hourlyData = res.list.map(element => {
-
           const date = new Date(1000 * (element.dt + res.city.timezone));
           return (
             {
               dayOfWeek: dayOfWeekText(date.getUTCDay()),
               time: readableTime(date.getUTCHours()),
-              temperature: Math.round((element.main.temp - 273.15) * (9/5) + 32),
+              temperature: Math.round((element.main.temp - 273.15) * (9 / 5) + 32),
               temperatureC: Math.round((element.main.temp - 273.15)),
               icon: `https://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png`,
               condition: element.weather[0].main,
@@ -46,15 +44,13 @@ export const findWeatherForecast = (zipCode) => {
               humidity: element.main.humidity
             }
           )
-
-
         })
 
         function readableTime(hours) {
-          if(hours > 0 && hours <= 12){
+          if (hours > 0 && hours <= 12) {
             return `${hours}AM`
-          } else if(hours >= 13 && hours < 24){
-            return `${hours-12}PM`
+          } else if (hours >= 13 && hours < 24) {
+            return `${hours - 12}PM`
           } else {
             return '12AM'
           }
@@ -62,20 +58,15 @@ export const findWeatherForecast = (zipCode) => {
 
         function dayOfWeekText(i) {
           const arr = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
-
           return arr[i]
         }
-
-
-      return (
-
-
-        {
-          zipCode: zipCode,
-          city: res.city.name,
-          hourlyWeatherArray: hourlyData
-        }
-      )}
+        return (
+          {
+            zipCode: zipCode,
+            city: res.city.name,
+            hourlyWeatherArray: hourlyData
+          }
+        )
+      }
     })
-
 }
